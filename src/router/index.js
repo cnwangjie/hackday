a11d51e9bbcc2d850a81731796a5293041cfbaa6
WangJie <i@i8e.net>
2018-06-02 16:48:53 +0800

    add deps & new pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from '@/pages/Login'
+import SelectPostion from '@/pages/SelectPostion'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      path: '/login',
+      component: Login,
+      name: 'login',
+    },
+    {
+      path: '/selectPostion',
+      component: SelectPostion,
+      name: 'select-postion',
+    },
   ]
 })
