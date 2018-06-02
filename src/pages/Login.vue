900a374fbb857fc2b6ccd656d561274436919b66
Unknown <584624960@qq.com>
2018-06-02 21:13:37 +0800

    Login,submit some pictures

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,7 +1,45 @@
 <template>
-<div>
 
+<div id="login1">
+  <div class="logo1">
+    <img src="/static/image/logo.png"
+         height="80px"
+         width="80px"
+         class="logo1"
+         />
+  </div>
+<h1>
+          选择您的登陆方式
+</h1>
+  <v-container fluid class="container1">
+    <v-layout row>
+      <v-flex xs8>
+        <v-text-field class="filed1"
+          name="input-1-3"
+          label="输入手机号读取验证码"
+        ></v-text-field>
+      </v-flex>
+      <v-flex xs5>
+        <v-btn small class="btn1">获取</v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
+  <div class="pictures">
+  <img src="/static/image/qq.png"
+       alt="QQ登陆"
+       height="80px"
+       width="80px"
+  />
+    <img src="/static/image/weixin.png"
+         alt="微信登陆"
+         height="80px"
+         width="80px"
+         class="p2"
+         />
+  </div>
+  <v-btn color="info" class="btn2">下一步</v-btn>
 </div>
+
 </template>
 <script>
 export default {
@@ -9,6 +47,37 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-
+  #login1{
+    margin-top:50px;
+  }
+  .logo1{
+    margin-left:20px
+  }
+h1{
+  margin-left:35px;
+  margin-top:50px;
+  color:#006
+}
+  .container{
+    text-align:center;
+    margin-left:15px;
+  }
+.btn{
+  margin-top:16px;
+}
+.field1{
+  margin-left:5px;
+}
+  .pictures{
+    position:relative;
+    margin-left:40px;
+    top:-5px;
+  }
+  .p2{
+    margin-left:20px;
+  }
+.btn2{
+  margin-left:40px;
+}
 </style>
 
